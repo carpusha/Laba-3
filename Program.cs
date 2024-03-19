ff071fae4b0ff8248f2f8dbc652f2454d3a66505
carpusha <blerdlol123@gmail.com>
2024-03-19 11:18:48 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp25;
+
+class Program
+{
+    static void Main()
+    {
+        Vector3D v1 = new Vector3D(1.5, 2.2, 3.8);
+        Vector3D v2 = new Vector3D(4.4, 5.5, 6.3);
+
+        Vector3D sum = v1.Add(v2);
+        Vector3D diff = v1.Subtract(v2);
+        double dotProduct = v1.DotProduct(v2);
+        double magnitudeV1 = v1.Magnitude();
+        double cosBetween = v1.CosineBetween(v2);
+        double magnitudeV2 = v2.Magnitude();
+
+        Console.WriteLine($"Сума: ({sum.X}, {sum.Y}, {sum.Z})");
+        Console.WriteLine($"Різниця: ({diff.X}, {diff.Y}, {diff.Z})");
+        Console.WriteLine($"Скалярний добуток: {dotProduct}");
+        Console.WriteLine($"Довжина V1: {magnitudeV1}");
+        Console.WriteLine($"Довжина V2:{magnitudeV2}");
+        Console.WriteLine($"Косинус кута між V1 та V2: {cosBetween}");
+
+        Console.WriteLine();
+
+        // Серіалізація
+        string filePath = @"D:\\Laba 3.txt";
+        v1.SerializeToJson(filePath);
+        Console.WriteLine("Вектор збережений в " + filePath);
+
+
+        // Десеріалізація
+        Vector3D deserializedVector = Vector3D.DeserializeFromJson(filePath);
+        Console.WriteLine($"Дисерілізований Вектор: {deserializedVector.X}, {deserializedVector.Y}, {deserializedVector.Z}");
+    }
+}
+
---
+++ Vector3D.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ConsoleApp25
+{
+    internal class Vector3D
+    {
+        private double x, y, z;
+
+        public Vector3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double X => x;
+        public double Y => y;
+        public double Z => z;
+
+        public Vector3D Add(Vector3D v)
+        {
+            return new Vector3D(x + v.x, y + v.y, z + v.z);
+        }
+
+        public Vector3D Subtract(Vector3D v)
+        {
+            return new Vector3D(x - v.x, y - v.y, z - v.z);
+        }
+
+        public double DotProduct(Vector3D v)
+        {
+            return x * v.x + y * v.y + z * v.z;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public double CosineBetween(Vector3D v)
+        {
+            return (DotProduct(v) / (Magnitude() * v.Magnitude()));
+        }
+
+        public void SerializeToJson(string filePath)
+        {
+            string json = JsonSerializer.Serialize(this);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static Vector3D DeserializeFromJson(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<Vector3D>(json);
+        }
+
+    }
+}
+
